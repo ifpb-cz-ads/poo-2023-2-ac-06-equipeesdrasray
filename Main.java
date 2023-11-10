791af9e431588462b7fb8c53b05a9b95626b99bf
Esdras <esdras@email.com>
2023-11-10 09:19:16 -0300

    Adicionando ui

--- Main.java
+++ Main.java
@@ -7,6 +7,30 @@ import dao.*;
 
 public class Main {
     public static void main(String...args) {
-        JOptionPane.showMessageDialog(null, "Hello World", "Titulo da mensagem", JOptionPane.ERROR_MESSAGE);
+        UsuarioDao dao = new UsuarioDao();
+        String options[] = {"salvar usuário", "Listar Usuários", "Buscar por email"};
+
+        String option = (String) JOptionPane.showInputDialog(
+                null,
+                "O que você deseja fazer?",
+                "Opção",
+                JOptionPane.QUESTION_MESSAGE,
+                null,
+                options,
+                options[0]
+                );
+
+        if(option.equals(options[0])){//salvar usuário
+            JOptionPane.showInputDialog("Email do usário");
+        } else if(option.equals(options[1])){//listar usuários
+            List<Usuario> usuariosLista = dao.listarUsuarios();
+            JOptionPane.showInputDialog(usuariosLista.toString()
+                    .replace("[", "")
+                    .replace("]", "")
+                    .replace(",", ""));
+        } else { //buscar por email
+
+        }
+
     }
 }
