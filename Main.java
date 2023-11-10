b48bde6c68b180e74af2b93cf095cd3448f2d3c6
Esdras Silva <esdraslimasilva83@gmail.com>
2023-11-10 20:28:18 -0300

    Update

--- Main.java
+++ Main.java
@@ -21,14 +21,40 @@ public class Main {
                 );
 
         if(option.equals(options[0])){//salvar usuário
-            JOptionPane.showInputDialog("Email do usário");
+            JTextField email = new JTextField();
+            JTextField senha = new JPasswordField();
+
+            Object[] message = {
+                    "email", email,
+                    "senha", senha
+            };
+
+            JOptionPane.showConfirmDialog(null, message, "Salvar usuario", JOptionPane.OK_OPTION);
+            boolean adicionado = dao.adicionarUsuario(new Usuario(email.getText(), senha.getText()));
+
+            if(!adicionado){
+                JOptionPane.showMessageDialog(null, "Erro ao salvar usuário", "Erro", JOptionPane.OK_OPTION);
+            } else {
+                JOptionPane.showMessageDialog(null, "Usuário adicionado!", "Confirmação", JOptionPane.OK_OPTION);
+            }
+
         } else if(option.equals(options[1])){//listar usuários
             List<Usuario> usuariosLista = dao.listarUsuarios();
-            JOptionPane.showInputDialog(usuariosLista.toString()
+            JOptionPane.showMessageDialog(null, usuariosLista.toString()
                     .replace("[", "")
                     .replace("]", "")
                     .replace(",", ""));
         } else { //buscar por email
+
+            String email = JOptionPane.showInputDialog(null, "email");
+
+            Usuario usuario = dao.buscarPorEmail(email);
+
+            if(usuario != null){
+                JOptionPane.showMessageDialog(null, usuario);
+            } else {
+                JOptionPane.showMessageDialog(null, "Usuário não encontrado");
+            }
 
         }
 
